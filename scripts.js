28352990a9659d67a2aad1de94d6c9ed41ac8e40
Flavia <flavia.bulad@gmail.com>
2022-04-04 17:21:44 -0300

    added green border to products when selected

--- scripts.js
+++ scripts.js
@@ -4,5 +4,23 @@ function selectDish(option1){
       dish.classList.toggle("dish-border");
    }
    option1.classList.add("dish-border");
-   order();
+   
  }
+ 
+ function selectDrink(option2){
+    let drink = document.querySelector(".drink-border");
+    if (drink !== null) {
+       drink.classList.toggle("drink-border");
+    }
+    option2.classList.add("drink-border");
+    
+  }
+ 
+  function selectDessert(option3){
+    let dessert = document.querySelector(".dessert-border");
+    if (dessert !== null) {
+       dessert.classList.toggle("dessert-border");
+    }
+    option3.classList.add("dessert-border");
+    
+  }
